3a06ee2d9fddff60041300418462f095eef51c80
Дмитрий <dmitrijadarov@gmail.com>
2025-07-18 14:31:06 +0300

    rar

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
-import rar
+import os,sys
 
-rgar = 5
+def say_hello( name ):
+  print("Hello, " +name+ "!")
+
+
+def unused_function():
+    pass
+
+def calculate_sum(a,b):
+    result=a + b
+    return result
+
+x= 10
+if x>5:
+ print("x is greater than 5")
+else:
+  print( "x is less or equal 5" )
+
