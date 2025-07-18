6234e337b062710be1feb2ae45052e126436162f
Дмитрий <dmitrijadarov@gmail.com>
2025-07-18 14:54:48 +0300

    rar

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
-import os,sys
-
-def say_hello( name ):
-  print("Hello, " +name+ "!")
-
-
-
+def say_hello(name):
+    print("Hello, " +name+ "!")
 
 def unused_function():
     pass
-
 
 def calculate_sum(a,b):
     result=a + b
     return result
 
-x= 10
-if x>5:
- print("x is greater than 5")
+x = 10
+if x>5 :
+    print("x is greater than 5")
 else:
-  print( "x is less or equal 5" )
+    print( "x is less or equal 5" )
 
